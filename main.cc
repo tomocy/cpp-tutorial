6bb3a360d4006bf726e2f630cbf4e6b43ba309ec
tomocy <tomocy.dev@gmail.com>
2020-04-27 22:50:53 +0900

    bin to dec

--- main.cc
+++ main.cc
@@ -1,11 +1,31 @@
 #include "all.h"
 
+template <typename T>
+T ReadFromStdin(std::string prompt) {
+  std::cout << prompt;
+  T v;
+  std::cin >> v;
+
+  return v;
+}
+
+int ConvertBinToDec(int bin) {
+  if (bin < 0) {
+    return -ConvertBinToDec(-bin);
+  }
+  if (bin <= 1) {
+    return bin;
+  }
+
+  return 2 * ConvertBinToDec(bin / 10) + bin % 10;
+}
+
 int main() {
-  for (auto i = 1; i <= 9; ++i) {
-    for (auto j = 1; j <= 9; ++j) {
-      std::cout << i * j << "\t"s;
-    }
-    std::cout << std::endl;
+  while (true) {
+    auto bin = ReadFromStdin<int>("binary > ");
+    auto dec = ConvertBinToDec(bin);
+    std::cout << "binary "s << bin << " is decimal "s << dec << "."s
+              << std::endl;
   }
 
   return EXIT_SUCCESS;
