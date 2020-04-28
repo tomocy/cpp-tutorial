7349ffc72f2001899a9e80f3d60232bca797cae8
tomocy <tomocy.dev@gmail.com>
2020-04-28 22:43:49 +0900

    sort with selection

--- main.cc
+++ main.cc
@@ -9,86 +9,53 @@ T ReadFromStdin(std::string prompt) {
   return v;
 }
 
-class Base {
- public:
-  Base(int val) : val_(val) {}
-
-  std::string String() const {
-    switch (val_) {
-      case 2:
-        return "binary";
-      case 10:
-        return "decimal";
-      default:
-        return "";
-    }
+std::vector<int> ReadIntsFromStdin(std::string prompt) {
+  std::vector<int> vs;
+  int v;
+  while ((v = ReadFromStdin<int>(prompt)) != 0) {
+    vs.push_back(v);
   }
 
-  int Value() const { return val_; }
-
- private:
-  int val_;
-};
-
-int ConvertBinToDec(int src) {
-  if (src < 0) {
-    return -ConvertBinToDec(-src);
-  }
-  if (src < 2) {
-    return src;
-  }
-
-  return 2 * ConvertBinToDec(src / 10) + src % 10;
+  return vs;
 }
 
-int ConvertDecToBin(int src) {
-  if (src < 0) {
-    return -ConvertDecToBin(-src);
+void SortWithSelection(std::vector<int>& vs) {
+  for (size_t i = 0; i < vs.size(); ++i) {
+    auto min = i;
+    for (size_t j = i + 1; j < vs.size(); ++j) {
+      if (vs.at(min) <= vs.at(j)) {
+        continue;
+      }
+
+      min = j;
+    }
+
+    auto tmp = vs.at(i);
+    vs.at(i) = vs.at(min);
+    vs.at(min) = tmp;
   }
-
-  std::function<std::string(std::string, int)> remainders =
-      [&remainders](std::string joined, int src) {
-        auto q = src / 2, r = src % 2;
-        joined += std::to_string(r);
-        if (q == 0) {
-          return joined;
-        }
-
-        return remainders(joined, q);
-      };
-  auto joined = remainders(""s, src);
-  std::reverse(joined.begin(), joined.end());
-
-  std::cout << joined << std::endl;
-
-  return std::stoi(joined);
 }
 
-void ConvertBaseFromStdin(Base from, Base to) {
-  auto src = ReadFromStdin<int>(from.String() + " > "s);
-  int converted = [from, to, src]() {
-    if (from.Value() == 2 && to.Value() == 10) {
-      return ConvertBinToDec(src);
+template <typename T>
+void PrintWithSep(std::vector<T> vs, std::string sep) {
+  for (size_t i = 0; i < vs.size(); ++i) {
+    std::cout << vs.at(i);
+    if (i != vs.size() - 1) {
+      std::cout << sep;
     }
-    if (from.Value() == 10 && to.Value() == 2) {
-      return ConvertDecToBin(src);
-    }
-
-    return 0;
-  }();
-
-  std::cout << from.String() << " "s << src << " is "s << to.String() << " "s
-            << converted << "."s << std::endl;
+  }
 }
 
 int main() {
-  std::cout << "convert bases" << std::endl;
-  std::cout << "SIGINT to quit" << std::endl;
-  auto from = Base(ReadFromStdin<int>("from > ")),
-       to = Base(ReadFromStdin<int>("to > "));
-  while (true) {
-    ConvertBaseFromStdin(from, to);
-  }
+  auto vs = ReadIntsFromStdin("integer > "s);
+
+  PrintWithSep<int>(vs, ", "s);
+  std::cout << " is sorted ";
+
+  SortWithSelection(vs);
+
+  PrintWithSep<int>(vs, ", "s);
+  std::cout << "."s << std::endl;
 
   return EXIT_SUCCESS;
 }
