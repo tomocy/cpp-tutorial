489f028711fff65a3478b1a257d5f1b8fc2788f5
tomocy <tomocy.dev@gmail.com>
2020-04-29 22:37:22 +0900

    operator overloading

--- main.cc
+++ main.cc
@@ -8,13 +8,46 @@ class Fraction {
     return static_cast<double>(num) / static_cast<double>(denom);
   }
 
+  Fraction operator+(const Fraction& right) {
+    if (denom == right.denom) {
+      return Fraction(num + right.num, denom);
+    }
+
+    return Fraction(num * right.denom + right.num * denom, denom * right.denom);
+  }
+
+  Fraction operator-(const Fraction& right) {
+    if (denom == right.denom) {
+      return Fraction(num - right.num, denom);
+    }
+
+    return Fraction(num * right.denom - right.num * denom, denom * right.denom);
+  }
+
+  Fraction operator*(const Fraction& right) {
+    return Fraction(num * right.num, denom * right.denom);
+  }
+
+  Fraction operator/(const Fraction& right) {
+    return Fraction(num * right.denom, denom * right.num);
+  }
+
  private:
   int num;
   int denom;
 };
 
 int main() {
-  auto f = Fraction(3, 2);
+  auto a = Fraction(6, 2);
+  auto b = Fraction(4, 2);
+  auto c = a + b;
+  auto d = a - b;
+  auto e = a * b;
+  auto f = a / b;
+
+  std::cout << c.Value() << std::endl;
+  std::cout << d.Value() << std::endl;
+  std::cout << e.Value() << std::endl;
   std::cout << f.Value() << std::endl;
 
   return EXIT_SUCCESS;
