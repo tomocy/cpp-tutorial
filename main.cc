427f072151b4595764abefe68a535587584b4d30
tomocy <tomocy.dev@gmail.com>
2020-04-29 22:18:28 +0900

    class

--- main.cc
+++ main.cc
@@ -1,14 +1,21 @@
 #include "all.h"
 
-void Print(const std::vector<int>& vs) {
-  for (auto v : vs) {
-    std::cout << v << std::endl;
+class Fraction {
+ public:
+  Fraction(int num, int denom) : num(num), denom(denom) {}
+
+  double Value() {
+    return static_cast<double>(num) / static_cast<double>(denom);
   }
-}
+
+ private:
+  int num;
+  int denom;
+};
 
 int main() {
-  auto vs = std::vector<int>{1, 3, 5, 2, 3, 1};
-  Print(vs);
+  auto f = Fraction(3, 2);
+  std::cout << f.Value() << std::endl;
 
   return EXIT_SUCCESS;
 }
