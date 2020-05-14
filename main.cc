ae8d6f18a6f995214395283ebd4e08b25761bae7
tomocy <tomocy.dev@gmail.com>
2020-05-14 22:13:41 +0900

    manual array

--- main.cc
+++ main.cc
@@ -1,54 +1,17 @@
 #include "all.h"
 
-class Fraction {
- public:
-  Fraction(int num, int denom) : num(num), denom(denom) {}
-
-  double Value() {
-    return static_cast<double>(num) / static_cast<double>(denom);
-  }
-
-  Fraction operator+(const Fraction& right) {
-    if (denom == right.denom) {
-      return Fraction(num + right.num, denom);
-    }
-
-    return Fraction(num * right.denom + right.num * denom, denom * right.denom);
-  }
-
-  Fraction operator-(const Fraction& right) {
-    if (denom == right.denom) {
-      return Fraction(num - right.num, denom);
-    }
-
-    return Fraction(num * right.denom - right.num * denom, denom * right.denom);
-  }
-
-  Fraction operator*(const Fraction& right) {
-    return Fraction(num * right.num, denom * right.denom);
-  }
-
-  Fraction operator/(const Fraction& right) {
-    return Fraction(num * right.denom, denom * right.num);
-  }
-
- private:
-  int num;
-  int denom;
+struct Array {
+  int& operator[](std::size_t i) { return data[i]; }
+  int data[10];
 };
 
 int main() {
-  auto a = Fraction(6, 2);
-  auto b = Fraction(4, 2);
-  auto c = a + b;
-  auto d = a - b;
-  auto e = a * b;
-  auto f = a / b;
-
-  std::cout << c.Value() << std::endl;
-  std::cout << d.Value() << std::endl;
-  std::cout << e.Value() << std::endl;
-  std::cout << f.Value() << std::endl;
+  auto a = Array{0, 1, 2, 3, 4, 5};
+  a[3] = 0;
+  auto size = sizeof(a) / sizeof(int);
+  for (std::size_t i = 0; i < size; ++i) {
+    std::cout << a[i] << std::endl;
+  }
 
   return EXIT_SUCCESS;
 }
