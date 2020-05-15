0fda56d2070faed67e18c29315be59611067aa94
tomocy <tomocy.dev@gmail.com>
2020-05-15 09:06:25 +0900

    Array with some methods

--- main.cc
+++ main.cc
@@ -7,14 +7,25 @@ struct Array {
 
   std::size_t size() const { return N; }
 
+  int& front() { return data[0]; }
+  const int& front() const { return data[0]; }
+
+  int& back() { return data[N - 1]; }
+  const int& back() const { return data[N - 1]; }
+
   T data[N];
 };
 
 int main() {
-  const auto a = Array<int, 20>{0, 1, 2, 3, 4, 5};
+  const auto a = Array<int, 20>{5, 4, 3, 2, 1};
   for (std::size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << std::endl;
   }
+
+  const auto front = a.front();
+  const auto back = a.back();
+  std::cout << "front: " << front << std::endl;
+  std::cout << "back: " << back << std::endl;
 
   return EXIT_SUCCESS;
 }
