a242969fd61071f8f0a1253da5be78fe24e29d76
tomocy <tomocy.dev@gmail.com>
2020-05-15 09:51:34 +0900

    Array with type aliases

--- main.cc
+++ main.cc
@@ -2,22 +2,25 @@
 
 template <typename T, std::size_t N>
 struct Array {
-  T& operator[](std::size_t i) { return data[i]; }
-  const T& operator[](std::size_t i) const { return data[i]; }
+  using value_type = T;
+  using reference = value_type&;
+
+  reference operator[](std::size_t i) { return data[i]; }
+  const reference operator[](std::size_t i) const { return data[i]; }
 
   std::size_t size() const { return N; }
 
-  int& front() { return data[0]; }
-  const int& front() const { return data[0]; }
+  reference front() { return data[0]; }
+  const reference front() const { return data[0]; }
 
-  int& back() { return data[N - 1]; }
-  const int& back() const { return data[N - 1]; }
+  reference back() { return data[N - 1]; }
+  const reference back() const { return data[N - 1]; }
 
-  T data[N];
+  value_type data[N];
 };
 
 int main() {
-  const auto a = Array<int, 20>{5, 4, 3, 2, 1};
+  auto a = Array<int, 20>{5, 4, 3, 2, 1};
   for (std::size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << std::endl;
   }
