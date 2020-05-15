99ef13b6779e8783df94cf557b65db1a29f36058
tomocy <tomocy.dev@gmail.com>
2020-05-15 14:48:24 +0900

    refactor ConstIter of Array

--- main.cc
+++ main.cc
@@ -120,66 +120,129 @@ bool Iter<Array>::operator>=(const Iter& right) const {
 
 template <typename Array>
 struct ConstIter {
-  ConstIter(const Array& arr, std::size_t i) : arr(arr), i(i) {}
+  ConstIter(const Array& arr, std::size_t i);
 
-  ConstIter(const typename Array::iterator iter) : arr(iter.arr), i(iter.i) {}
+  ConstIter(const typename Array::iterator iter);
 
-  typename Array::const_reference operator*() const { return arr[i]; }
+  typename Array::const_reference operator*() const;
 
-  ConstIter& operator++() {
-    ++i;
-    return *this;
-  }
+  typename Array::const_reference operator[](std::size_t n) const;
 
-  ConstIter operator++(int) {
-    auto copied = *this;
-    ++*this;
-    return copied;
-  }
+  typename Array::const_iterator& operator++();
 
-  ConstIter& operator+=(std::size_t n) {
-    i += n;
-    return *this;
-  }
+  typename Array::const_iterator operator++(int);
 
-  ConstIter operator+(std::size_t n) const {
-    auto copied = *this;
-    copied += n;
-    return copied;
-  }
+  typename Array::const_iterator& operator+=(std::size_t n);
 
-  ConstIter& operator--() {
-    --i;
-    return *this;
-  }
+  typename Array::const_iterator operator+(std::size_t n) const;
 
-  ConstIter operator--(int) {
-    auto copied = *this;
-    --*this;
-    return copied;
-  }
+  typename Array::const_iterator& operator--();
 
-  bool operator==(const ConstIter& right) {
-    return i == right.i && arr[i] == *right;
-  }
+  typename Array::const_iterator operator--(int);
 
-  bool operator!=(const ConstIter& right) { return !(*this == right); }
+  bool operator==(const ConstIter& right);
 
-  bool operator<(const ConstIter& right) { return i < right.i; }
+  bool operator!=(const ConstIter& right);
 
-  bool operator<=(const ConstIter& right) { return i <= right.i; }
+  bool operator<(const ConstIter& right);
 
-  bool operator>(const ConstIter& right) { return i > right.i; }
+  bool operator<=(const ConstIter& right);
 
-  bool operator>=(const ConstIter& right) { return i >= right.i; }
+  bool operator>(const ConstIter& right);
 
-  typename Array::const_reference operator[](std::size_t n) const {
-    return *(*this + n);
-  }
+  bool operator>=(const ConstIter& right);
 
   const Array& arr;
   std::size_t i;
 };
+
+template <typename Array>
+ConstIter<Array>::ConstIter(const Array& arr, std::size_t i) : arr(arr), i(i) {}
+
+template <typename Array>
+ConstIter<Array>::ConstIter(const typename Array::iterator iter)
+    : arr(iter.arr), i(iter.i) {}
+
+template <typename Array>
+typename Array::const_reference ConstIter<Array>::operator*() const {
+  return arr[i];
+}
+
+template <typename Array>
+typename Array::const_reference ConstIter<Array>::operator[](
+    std::size_t n) const {
+  return *(*this + n);
+}
+
+template <typename Array>
+typename Array::const_iterator& ConstIter<Array>::operator++() {
+  ++i;
+  return *this;
+}
+
+template <typename Array>
+typename Array::const_iterator ConstIter<Array>::operator++(int) {
+  auto copied = *this;
+  ++*this;
+  return copied;
+}
+
+template <typename Array>
+typename Array::const_iterator& ConstIter<Array>::operator+=(std::size_t n) {
+  i += n;
+  return *this;
+}
+
+template <typename Array>
+typename Array::const_iterator ConstIter<Array>::operator+(
+    std::size_t n) const {
+  auto copied = *this;
+  copied += n;
+  return copied;
+}
+
+template <typename Array>
+typename Array::const_iterator& ConstIter<Array>::operator--() {
+  --i;
+  return *this;
+}
+
+template <typename Array>
+typename Array::const_iterator ConstIter<Array>::operator--(int) {
+  auto copied = *this;
+  --*this;
+  return copied;
+}
+
+template <typename Array>
+bool ConstIter<Array>::operator==(const ConstIter& right) {
+  return i == right.i && arr[i] == *right;
+}
+
+template <typename Array>
+bool ConstIter<Array>::operator!=(const ConstIter& right) {
+  return !(*this == right);
+}
+
+template <typename Array>
+bool ConstIter<Array>::operator<(const ConstIter& right) {
+  return i < right.i;
+}
+
+template <typename Array>
+bool ConstIter<Array>::operator<=(const ConstIter& right) {
+  return i <= right.i;
+}
+
+template <typename Array>
+bool ConstIter<Array>::operator>(const ConstIter& right) {
+  return i > right.i;
+}
+
+template <typename Array>
+bool ConstIter<Array>::operator>=(const ConstIter& right) {
+  return i >= right.i;
+}
 
 template <typename T, std::size_t N>
 struct Array {
